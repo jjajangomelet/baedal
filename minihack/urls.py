1ff4048d340591a47a26fd36dad89937278aa793
jjajangomelet <35620908+jjajangomelet@users.noreply.github.com>
2019-05-28 20:45:27 +0900

    urls.py changed

--- minihack/urls.py
+++ minihack/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+import accounts as accounts
+import accounts as accounts
 
 urlpatterns = [
     path('admin/', admin.site.urls),
