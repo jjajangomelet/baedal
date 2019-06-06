e15a1ba1dd7b667b454c3ea204561d3fc4480bb0
jjajangomelet <35620908+jjajangomelet@users.noreply.github.com>
2019-06-06 13:26:17 +0900

    url connecting

--- orders/views.py
+++ orders/views.py
@@ -3,32 +3,23 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 
 from .models import Order, UserOrderDetail
-from .forms import OrderForm
+
 from datetime import datetime
 
 # Create your views here.
 def index(request):
-    orders = Order.objects.all('-pk')
-    userOrderDetail = UserOrderDetail.objects.all('-pk')
-    nowTime = datetime.now()
-    ctx = {
-        'orders': orders,
-        'userOrderDeatils': userOrderDetail,
-        'nowTime' : nowTime,
-    }
-    return render(request, 'orders/index.html', ctx)
+    return render(request, "orders/index.html")
 
 
-@login_required
+# @login_required
 def create(request):
-    if request.method == 'POST':
-        order_form = OrderForm(request.POST)
-        if order_form.is_valid():
-            order = order_form.save(commit=False)
-            order.host = request.user
-            order.save()
-    else:
-        return redirect("")
+    return redirect("")
 
 def read(request):
-    order = Order.objects.all('-pk')
+    return render(request, "orders/read.html")
+
+def new(request):
+    return render(request, "orders/new.html")
+
+def detail(request):
+    return render(request, "orders/detail.html")
